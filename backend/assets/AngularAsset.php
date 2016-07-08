feb9316dac2f2193c8487a33c1f930029c7ed5ad
hughshen <hughssss@outlook.com>
2016-07-09 02:49:26 +0800

    use cdn

--- backend/assets/AngularAsset.php
+++ backend/assets/AngularAsset.php
@@ -12,8 +12,8 @@ class AngularAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $js = [
-        'node_modules/angular/angular.min.js',
-        'node_modules/angular-route/angular-route.min.js',
+        'https://cdn.bootcss.com/angular.js/1.5.7/angular.min.js',
+        'https://cdn.bootcss.com/angular.js/1.5.7/angular-route.min.js',
     ];
     public $jsOptions = [
         'position' => \yii\web\View::POS_HEAD,
--- frontend/web/js/app.js
+++ frontend/web/js/app.js
@@ -1,6 +1,5 @@
 var app = angular.module('app', [
     'ngRoute',
-    'mgcrea.ngStrap'
 ]);
 
 app.config(['$routeProvider',
